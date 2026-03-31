49056c585301b8c4334528164b545bf72354f97c
Adriel Yañez <adr.yanez@duocuc.cl>
2026-03-31 14:30:11 -0300

    Add files via upload

---
+++ adr/src/main/java/cl/yanez/adr/AdrApplication.java
@@ -0,0 +1,13 @@
+package cl.yanez.adr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrApplication.class, args);
+	}
+
+}
